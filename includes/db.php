2c67d677b3ef1e2a97661a4618248c7c4526b639
asophben <34121496+asophben@users.noreply.github.com>
2017-12-13 17:47:03 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,23 @@
+<?php
+  
+  $host = 'localhost';
+  $user = 'root';
+  $pass = '';
+  $name = 'db_name';
 
+  db["db_host"] = $host;
+  db["db_user"] = $user;
+  db["db_pass"] = $pass;
+  db["db_name"] = $name;
+
+  foreach ( $db as $key => $value){
+    define(strtoupper($key),$value);
+  }
+
+  $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+  if(!$connection){
+      die('Unable to connect to Server/Database');
+  }
+  echo 'Connected to database Successfully';
+  
+?>
